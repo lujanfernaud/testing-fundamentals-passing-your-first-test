50769043c93da57fdb014f2503e54dd780bd320d
lujanfernaud <lujanfernaud@gmail.com>
2017-12-07 09:16:56 +0000

    Add spec for Person#full_name

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -3,9 +3,12 @@ require_relative "../lib/person"
 describe Person do
   describe "#full_name" do
     it "returns the first and last names concatenated" do
-      # Write your spec here.
-      # You can delete the pending line.
-      pending "Not written yet"
+      first_name = "Dong"
+      last_name  = "Huang"
+      full_name  = first_name + " " + last_name
+      person = described_class.new(first_name: first_name, last_name: last_name)
+
+      expect(person.full_name).to eq(full_name)
     end
   end
 end
