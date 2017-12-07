ea659f83a15cb574768632e36bd4f28fcc90338d
lujanfernaud <lujanfernaud@gmail.com>
2017-12-07 09:21:21 +0000

    Refactor spec for Person#full_name

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -3,12 +3,9 @@ require_relative "../lib/person"
 describe Person do
   describe "#full_name" do
     it "returns the first and last names concatenated" do
-      first_name = "Dong"
-      last_name  = "Huang"
-      full_name  = first_name + " " + last_name
-      person = described_class.new(first_name: first_name, last_name: last_name)
+      person = described_class.new(first_name: "Dong", last_name: "Huang")
 
-      expect(person.full_name).to eq(full_name)
+      expect(person.full_name).to eq("Dong Huang")
     end
   end
 end
